3640c876e7ac986cd1cfcd66821766cb0acf7fba
alex <enotasd@gmail.com>
2023-04-15 21:36:56 +0300

    hw-1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+from csv import DictReader
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +15,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +26,36 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
+        return self.calculate_total_price() * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(self):
+        """
+        Класс-метод, инициализирующий экземпляры класса Item
+        данными из файла src/items.csv
+        """
         pass
+
+    @staticmethod
+    def string_to_number(value):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        return int(float(value))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            print("Exception: Длина наименования товара превышает 10 символов.")
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 5)
+item2 = Item("Ноутбук", 50000, 2)
+Item.pay_rate = 0.9
+def test_calculate_total_price():
+    '''
+    Тест на проверку функции get_filtered_data на количество значений EXECUTED
+    '''
+    print()
+    assert item1.calculate_total_price() == 50000
+    assert item2.calculate_total_price() == 100000
+
+def test_apply_discount():
+    '''
+    Тест на проверку функции get_filtered_data на количество значений EXECUTED
+    '''
+    assert item1.apply_discount() == 45000
+    assert item2.apply_discount() == 90000
+
